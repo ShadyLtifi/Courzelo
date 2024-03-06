36314e4d4294a662b601709f94f89415dc790f79
loua <loua.jday@gmail.com>
2024-03-06 20:48:42 +0100

    crud complet

---
+++ Courzelo_Angular/src/app/Claim/updateclaim/updateclaim.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-updateclaim',
+  templateUrl: './updateclaim.component.html',
+  styleUrls: ['./updateclaim.component.css']
+})
+export class UpdateclaimComponent {
+
+}
